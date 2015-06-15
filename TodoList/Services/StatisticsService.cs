3002a0099ecb209a9351e2617ef66efcf3d0a252
Alex Bogomol <alexander.bogomol@gmail.com>
2015-06-15 23:49:09 +0300

    Injecting a service into a view

---
+++ TodoList/Services/StatisticsService.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Models;
+
+namespace TodoList.Services
+{
+    public class StatisticsService
+    {
+        private readonly ApplicationDbContext db;
+
+        public StatisticsService(ApplicationDbContext context)
+        {
+            db = context;
+        }
+
+        public async Task<int> GetCount()
+        {
+            return await Task.FromResult(db.TodoItems.Count());
+        }
+
+        public async Task<int> GetCompletedCount()
+        {
+            return await Task.FromResult(db.TodoItems.Count(x => x.IsDone == true));
+        }
+
+        public async Task<double> GetAveragePriority()
+        {
+            if (db.TodoItems.Count() == 0)
+            {
+                return 0.0;
+            }
+
+            return await Task.FromResult(db.TodoItems.Average(x => x.Priority));
+        }
+    }
+}
--- TodoList/Startup.cs
+++ TodoList/Startup.cs
@@ -1,25 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Authentication.Facebook;
-using Microsoft.AspNet.Authentication.Google;
+﻿using Microsoft.AspNet.Authentication.Facebook;
 using Microsoft.AspNet.Authentication.MicrosoftAccount;
-using Microsoft.AspNet.Authentication.Twitter;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Diagnostics;
 using Microsoft.AspNet.Diagnostics.Entity;
 using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
-using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.AspNet.Routing;
 using Microsoft.Data.Entity;
 using Microsoft.Framework.ConfigurationModel;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.Logging;
-using Microsoft.Framework.Logging.Console;
-using Microsoft.Framework.Runtime;
 using TodoList.Models;
 
 namespace TodoList
@@ -83,6 +72,8 @@ namespace TodoList
             // Uncomment the following line to add Web API services which makes it easier to port Web API 2 controllers.
             // You will also need to add the Microsoft.AspNet.Mvc.WebApiCompatShim package to the 'dependencies' section of project.json.
             // services.AddWebApiConventions();
+
+            services.AddTransient<Services.StatisticsService>();
         }
 
         // Configure is called after ConfigureServices is called.
