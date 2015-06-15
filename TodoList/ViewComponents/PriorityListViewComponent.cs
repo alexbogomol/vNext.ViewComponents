544d74e8de2620f51f6e3471f5ab795a4c3dad15
Alex Bogomol <alexander.bogomol@gmail.com>
2015-06-15 23:07:47 +0300

    Adding a view-component class and view (sync mode)

---
+++ TodoList/ViewComponents/PriorityListViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Mvc;
+using System.Linq;
+using TodoList.Models;
+
+namespace TodoList.ViewComponents
+{
+    public class PriorityListViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext db;
+
+        public PriorityListViewComponent(ApplicationDbContext context)
+        {
+            db = context;
+        }
+
+        public IViewComponentResult Invoke(int maxPriority)
+        {
+            var items = db.TodoItems.Where(x => x.IsDone == false &&
+                                           x.Priority <= maxPriority);
+
+            return View(items);
+        }
+    }
+}
